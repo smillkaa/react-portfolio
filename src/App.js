a83bb62089763c2e30e86a70c8c3b1406c147587
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-09-17 16:31:54 -0700

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./assets/components/Header";
+import Nav from "./assets/components/Navigation";
+import Project from "./assets/components/Project";
+import Footer from "./assets/components/Footer";
+
+function App() {
+  return (
+    <Header>
+      <Nav></Nav>
+    </Header>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Project/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Project() {
+    return (
+        <section>
+            <div>
+                
+            </div>
+
+        </section>
+    );
+};
+
+export default Project;
